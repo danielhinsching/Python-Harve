a5cbdab45f7c03b646d69ed714b030ac21c85601
danielhinsching <danielsantoshinsching@gmail.com>
2026-09-17 19:29:02 -0300

    Adiciona dicionário com os 10 países mais populosos e suas populações aproximadas

---
+++ aula 4/paises.py
@@ -0,0 +1,13 @@
+# Dicionário com os 10 países mais populosos (população aproximada)
+populacao_paises = {
+    "Índia": 1428000000,
+    "China": 1425000000,
+    "Estados Unidos": 339000000,
+    "Indonésia": 277000000,
+    "Paquistão": 240000000,
+    "Nigéria": 223000000,
+    "Brasil": 216000000,
+    "Bangladesh": 173000000,
+    "Rússia": 144000000,
+    "México": 128000000,
+}
